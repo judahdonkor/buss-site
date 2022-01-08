ec77fcecd5074f1f2fef6b21c201d60ad412c986
judahdonkor <judahdonkor@gmail.com>
2022-01-08 20:47:29 +0000

    Add table to commitment page

--- src/pages/commitment.tsx
+++ src/pages/commitment.tsx
@@ -1,10 +1,13 @@
-import { Entity } from '@judahdonkor/chassis-client-es/types/repository'
+import moment from 'moment'
 import * as tsx from 'vue-tsx-support'
+import { mdl, merge } from '~/commitment'
 import {
   Breadcrumb,
-  Level,
+  Card,
+  DataTable,
+  DataTableColumnProps,
+  Level
 } from '~/components'
-import { merge } from '~/commitment'
 import { Dialog } from '~/mixins'
 import { EventHub } from '~/toolkit'
 
@@ -38,6 +41,79 @@ export default tsx.componentFactory
             })}>
               Create New Commitment
             </b-button>
+          </section>
+          <section class="section has-default-padding-top">
+            <Card class="has-table">
+              <b-switch
+                type="is-primary"
+                value={this.active}
+                onInput={(val: boolean) => (this.active = val)}
+              >
+                Active
+              </b-switch>
+              <DataTable
+                paginated
+                mdl={mdl}
+                where={[
+                  {
+                    type: 'EQUAL',
+                    params: {
+                      x: 'active',
+                      y: this.active,
+                    },
+                  },
+                ]}
+                onEventHub={(eventhub) => (this.tableEventHub = eventhub)}
+              >
+                <b-table-column
+                  label="ID"
+                  numeric
+                  scopedSlots={{
+                    default: ({ row }: DataTableColumnProps) => row?.id,
+                  }}
+                />
+                <b-table-column
+                  label="Date"
+                  scopedSlots={{
+                    default: ({ row }: DataTableColumnProps) =>
+                      moment(row.createdAt).format('ll'),
+                  }}
+                />
+                {/* <b-table-column
+                  numeric
+                  scopedSlots={{
+                    default: ({ row }: DataTableColumnProps) => (
+                      <div class="buttons has-addons is-right">
+                        {!row.active && (
+                          <b-button
+                            icon-left="edit"
+                            onClick={() => this.merge(row)}
+                          />
+                        )}
+                        <b-button
+                          onClick={async () => {
+                            try {
+                              await confirmReceive({ ctx: this, receive: row, fullScreen: true })
+                              this.$notify({
+                                context: 'Receipt',
+                                message: `${row.active ? 'Deactivated' : 'Activated'
+                                  } ${row.display}`,
+                                state: 'success',
+                              })
+                              this.tableEventHub?.refresh()
+                            } catch (error) {
+                              this.$notifyError(error)
+                            }
+                          }}
+                        >
+                          {row.active ? 'Deactivate' : 'Activate'}
+                        </b-button>
+                      </div>
+                    ),
+                  }}
+                /> */}
+              </DataTable>
+            </Card>
           </section>
         </div>
       )
